dbd77ace3736259e4a2e5f98cb2ba8ca1d3fab8e
Andriikryl <andreyk557@gmail.com>
2023-06-06 17:36:22 +0300

    add styles

--- src/app/page.js
+++ src/app/page.js
@@ -3,16 +3,21 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.container}>
-      <div className={styles.body}></div>
+      <div className={styles.body}>
+        <Text3D primary={"Atlason"} secondary={"Philosophy"} />
+        <Text3D primary={"Tudio"} secondary={"Team"} />
+        <Text3D primary={"Furniture"} secondary={"Beautiful"} />
+        <Text3D primary={"Products"} secondary={"Useful"} />
+      </div>
     </main>
   );
 }
 
 function Text3D({ primary, secondary }) {
   return (
-    <div className="styles.textContainer">
-      <p>{primary}</p>
-      <p>{secondary}</p>
+    <div className={styles.textContainer}>
+      <p className={styles.primary}>{primary}</p>
+      <p className={styles.secondary}>{secondary}</p>
     </div>
   );
 }
